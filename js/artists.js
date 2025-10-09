a00820634bfe4c03dc00e8ced31bacde81be00bb
Thomaspupu <thomas.pujol0407@gmail.com>
2025-10-09 22:09:44 +0200

    added picture for everyone, we can change image and description by clicking the cards (object js + function)

--- js/artists.js
+++ js/artists.js
@@ -10,7 +10,7 @@ const artists = {
     track3: "STILL D.R.E",
     album3: "'2001'",
     release3: "1999",
-    image: "/images/thomas.png",
+    image: "assets/images/portrait-thomas.png",
   },
   juliette: {
     name: "JULIETTE",
@@ -23,7 +23,7 @@ const artists = {
     track3: "POUR ELLE",
     album3: "A la vie a la mort",
     release3: "2024",
-    image: "",
+    image: "assets/images/portrait-juliette.png",
   },
   maxime: {
     name: "MAXIME",
@@ -36,7 +36,7 @@ const artists = {
     track3: "",
     album3: "",
     release3: "",
-    image: "",
+    image: "assets/images/portrait-maxime.png",
   },
   romain: {
     name: "ROMAIN",
@@ -49,7 +49,7 @@ const artists = {
     track3: "",
     album3: "",
     release3: "",
-    image: "",
+    image: "assets/images/portrait-romain.png",
   },
 
   joel: {
@@ -63,7 +63,7 @@ const artists = {
     track3: "",
     album3: "",
     release3: "",
-    image: "",
+    image: "assets/images/portrait-joel.png",
   },
 
   teddy: {
@@ -77,7 +77,7 @@ const artists = {
     track3: "",
     album3: "",
     release3: "",
-    image: "",
+    image: "assets/images/portrait-teddy.png",
   },
   cecile: {
     name: "CECILE",
@@ -90,15 +90,74 @@ const artists = {
     track3: "",
     album3: "",
     release3: "",
-    image: "",
+    image: "assets/images/portrait-cecile.png",
   },
 };
 
-function artistUpdate() {
+function artistUpdate(firstname) {
+  const artist = artists[firstname]
   const nameToUpdate = document.getElementById("artistName");
-  nameToUpdate.innerHTML = artists.juliette.name;
-  const trackToUpdate = 
+  nameToUpdate.innerHTML = artist.name;
+  const track1ToUpdate = document.getElementById("track1");
+  track1ToUpdate.innerHTML = artist.track1;
+  const album1ToUpdate = document.getElementById("album1");
+  album1ToUpdate.innerHTML = artist.album1;
+  const release1ToUpdate = document.getElementById("release1")
+  release1ToUpdate.innerHTML = artist.release1;
+
+  const track2ToUpdate = document.getElementById("track2");
+  track2ToUpdate.innerHTML = artist.track2;
+  const album2ToUpdate = document.getElementById("album2");
+  album2ToUpdate.innerHTML = artist.album2;
+  const release2ToUpdate = document.getElementById("release2")
+  release2ToUpdate.innerHTML = artist.release2;
+
+  const track3ToUpdate = document.getElementById("track3");
+  track3ToUpdate.innerHTML = artist.track3;
+  const album3ToUpdate = document.getElementById("album3");
+  album3ToUpdate.innerHTML = artist.album3;
+  const release3ToUpdate = document.getElementById("release3");
+  release3ToUpdate.innerHTML = artist.release3;
+
+  const imgToUpdate = document.getElementById("profileimg");
+  imgToUpdate.src = artist.image;
+
 }
 
 const cardJuliette = document.getElementById("card-juliette");
-cardJuliette.addEventListener("click", artistUpdate);
+cardJuliette.addEventListener("click", ()=>{
+  artistUpdate("juliette")
+});
+
+const cardThomas = document.getElementById("card-thomas");
+cardThomas.addEventListener("click", ()=>{
+  artistUpdate("thomas")
+});
+
+const cardTeddy = document.getElementById("card-teddy");
+cardTeddy.addEventListener("click", ()=>{
+  artistUpdate("teddy")
+});
+
+const cardCecile = document.getElementById("card-cecile");
+cardCecile.addEventListener("click", ()=>{
+  artistUpdate("cecile")
+});
+
+const cardRomain = document.getElementById("card-romain");
+cardRomain.addEventListener("click", ()=>{
+  artistUpdate("romain")
+});
+
+const cardMaxime = document.getElementById("card-maxime");
+cardMaxime.addEventListener("click", ()=>{
+  artistUpdate("maxime")
+});
+
+const cardJoel = document.getElementById("card-joel");
+cardJoel.addEventListener("click", ()=>{
+  artistUpdate("joel")
+});
+
+
+
